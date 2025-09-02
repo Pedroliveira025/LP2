f616bb8ad62a64fa79b0f883a84f0f15c736d9a8
Pedroliveira025 <pedro.oliveira.025@ufrn.edu.br>
2025-09-02 22:53:22 +0000

    atividade

---
+++ atividade_05.java
@@ -0,0 +1,42 @@
+
+class Pessoa
+{
+	private String nome;
+
+	public void apresentar()
+	{
+
+	}
+}
+
+class Aluno extends Pessoa
+{
+	private String matrícula;
+
+	@Override public void apresentar()
+	{
+
+	}
+
+}
+
+class Professor extends Pessoa
+{
+	private String disciplina;
+
+	@Override public void apresentar()
+	{
+
+	}
+}
+
+class Funcionário extends Pessoa
+{
+	private String cargo;
+
+	@Override public void apresentar()
+	{
+
+	}
+}
+
